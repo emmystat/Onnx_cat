a2d9dd247694a69334d9b6db6a7ad5d91957d4d4
emmystat <emnifz@gmail.com>
2026-06-28 19:53:01 +0100

    third

--- train_onnx.py
+++ train_onnx.py
@@ -2,7 +2,7 @@ from sklearn.datasets import make_classification
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 import onnxmltools
-from onnxmltools.convert.commot.data_types import FloatTensorType
+from onnxmltools.convert.common.data_types import FloatTensorType
 X, y = make_classification(n_samples=1000, n_features=4, random_state=42)
 
 X_train, X_test, y_train, y_test = train_test_split(
